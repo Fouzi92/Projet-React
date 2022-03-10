03c1a3d69ba9887adf8af7873d5d431e9fccf388
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-03-10 17:58:02 +0100

    suite

---
+++ pays/src/pages/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>À propos</h1> <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos vel,
+        aliquam dolore pariatur adipisci architecto corrupti, consectetur nobis
+        asperiores excepturi accusamus totam corporis officiis repudiandae odio
+        omnis. Soluta, quas! Dolor, repellendus, amet quos debitis officia ipsum
+        maiores quaerat ab odit earum mollitia tenetur nam soluta perferendis
+        architecto officiis quis animi est impedit cupiditate suscipit itaque
+        laboriosam ullam? Ad, nihil hic!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolorum
+        omnis, tempore temporibus dignissimos aperiam veniam similique odio
+        voluptas doloremque, illum pariatur ad nam placeat perferendis error!
+        Officiis excepturi atque ipsum voluptate alias veniam praesentium omnis
+        repellendus magnam, iure ratione vero. Minus dolore asperiores
+        consequatur dignissimos rem illum necessitatibus neque eveniet atque.
+        Suscipit aliquid atque ipsa cum est. Eos officia quos commodi possimus
+        sint quidem itaque repellat aut distinctio odio error voluptatem, nisi
+        assumenda iste dolores maiores tempora voluptatum unde alias fuga! Sunt
+        doloribus suscipit unde rerum, tempora eum modi sapiente maxime,
+        laudantium non tenetur cum est temporibus magni. Iusto?
+      </p>
+    </div>
+  );
+};
+
+export default About;
