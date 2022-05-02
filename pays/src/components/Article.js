a2903eb3622389b91e6d92e0a6673ce471def08c
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-02 13:03:12 +0200

    suite

--- pays/src/components/Article.js
+++ pays/src/components/Article.js
@@ -1,11 +1,28 @@
 import React from "react";
 
 const Article = ({ article }) => {
-  console.log(article);
+  const dateFormater = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    return newDate;
+  };
+
   return (
     <div className="article">
       <div className="card-header">
-        <h3>Article</h3>
+        <h3>{article.author}</h3>
+        <em>Posté le {dateFormater(article.date)}</em>
+      </div>
+      <p>{article.content}</p>
+      <div className="btn-container">
+        <button>Edit</button>
+        <button>Suprimer</button>
       </div>
     </div>
   );
--- pays/src/pages/Blog.js
+++ pays/src/pages/Blog.js
@@ -6,6 +6,7 @@ import Navigation from "../components/Navigation";
 
 const Blog = () => {
   const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
 
@@ -22,6 +23,11 @@ const Blog = () => {
     if (content.length < 140) {
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author: "author",
+        content,
+        date: Date.now(),
+      });
       setError(false);
     }
   };
@@ -32,7 +38,11 @@ const Blog = () => {
       <Navigation />
       <h1>Logo</h1>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+        />
         <textarea
           style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
@@ -42,9 +52,11 @@ const Blog = () => {
         <input type="submit" value="Envoyer" />
       </form>
       <ul>
-        {blogData.map((article) => (
-          <Article key={article.id} article={article} />
-        ))}
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
       </ul>
     </div>
   );
